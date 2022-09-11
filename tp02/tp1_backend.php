4ec0838eee8cc4ab7b9bd0994b88064bc5559af4
Miguel Angel Coletto <miguelcoletto@gmail.com>
2022-09-11 18:54:56 -0300

    commit 1

---
+++ tp02/tp1_backend.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<h1>Trabajo Practico 1</h1>
+
+  <?php
+    $saludo= "Hola Potrero";
+    echo $saludo;
+
+    $número1 = 70
+    $número2 = 6
+
+    $suma = $número1 + $número2;
+    echo $suma;
+    
+    echo $número1 + $número2;
+
+   ?>
+
+
+  </body>
+</html>
